85c9cda0c49d9044667320bdbcd9c03902319a18
HAKJOON0223 <2hakjoon@gmail.com>
2022-09-05 22:00:23 +0900

    change attribute name of path element into camelCase

--- components/hamburguer-menu.tsx
+++ components/hamburguer-menu.tsx
@@ -23,9 +23,9 @@ const HamburguerMenu: FunctionComponent<HamburguerMenuProps> = (props) => {
                 aria-hidden="true"
             >
                 <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M4 6h16M4 12h16M4 18h16"
                 />
             </svg>
@@ -38,9 +38,9 @@ const HamburguerMenu: FunctionComponent<HamburguerMenuProps> = (props) => {
                 aria-hidden="true"
             >
                 <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M6 18L18 6M6 6l12 12"
                 />
             </svg>
